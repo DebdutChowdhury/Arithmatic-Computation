acb82b7dbd3033d3467483668cd1f5fad3beb81f
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-13 20:37:14 +0530

    Added: Second compute all user input

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -4,5 +4,7 @@ read -p "Enter the number of a: " a
 read -p "Enter the number of b: " b
 read -p "Enter the number of c: " c
 
-compute=$(( a+(b*c)))
-echo $compute
+compute_01=$((a+(b*c)))
+compute_02=$((a*(b+c)))
+# echo $compute_01
+echo $compute_02
