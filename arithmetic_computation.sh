b6b03fcde66762b44e244da7e31d6a53963c84d3
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-13 22:00:04 +0530

    Added: Stored all value in the dictionary

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/bin/bash -x
+
+declare -A return
+
+case=1
 
 read -p "Enter the number of a: " a
 read -p "Enter the number of b: " b
 read -p "Enter the number of c: " c
 
 compute_01=$((a+(b*c)))
-compute_02=$((a*(b+c)))
-compute_03=`echo "scale=2; $c+$a/$b" | bc -l`
-compute_04=`echo "scale=2; $a%$b+$c" | bc -l`
+compute_02=$((c+(a*b)))
+compute_03=$((c+(a/b)))
+# compute_03=`echo "scale=2; $c+$a/$b" | bc -l`
+compute_04=$((c+(a%b)))
+#compute_04=`echo "scale=2; $c+$a%$b" | bc -l`
 
-# echo $compute_01
-# echo $compute_02
-# echo $compute_03
-echo $compute_04
+return[$((case++))]="$compute_01"
+return[$((case++))]="$compute_02"
+return[$((case++))]="$compute_03"
+return[$((case++))]="$compute_04"
+
+echo cases: ${!return[@]}
+echo value: ${return[@]}
