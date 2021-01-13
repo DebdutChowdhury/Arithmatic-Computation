441e1df27e1eee0145100bced6ca05ca24e1f36f
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-13 20:56:35 +0530

    Added: Third compute all user input

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -6,5 +6,8 @@ read -p "Enter the number of c: " c
 
 compute_01=$((a+(b*c)))
 compute_02=$((a*(b+c)))
+compute_03=`echo "scale=2; $c+$a/$b" | bc -l`
+
 # echo $compute_01
-echo $compute_02
+# echo $compute_02
+echo $compute_03
